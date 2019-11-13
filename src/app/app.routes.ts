a612aee753454acf85892a0ef5bd8e53cfb41597
dimkalobov95 <dimkalobov95@gmail.com>
2019-11-13 11:05:12 +0200

    initial module structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const APP_ROUTES: Routes = [
+  { path: '*', loadChildren: './pages/home/home.module#HomeModule'},
+  { path: 'tours', loadChildren: './pages/tours/tours.module#ToursModule'},
+  { path: 'team', loadChildren: './pages/team/team.module#TeamModule'},
+  { path: 'blog', loadChildren: './pages/blog/blog.module#BlogModule'},
+  { path: 'contacts', loadChildren: './pages/contacts/contacts.module#ContactsModule'},
+  { path: 'join', loadChildren: './pages/sign/sign.module#SignModule'},
+  { path: 'reviews', loadChildren: './pages/reviews/reviews.module#ReviewsModule'}
+];
