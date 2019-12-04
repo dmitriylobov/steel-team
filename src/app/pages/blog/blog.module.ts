1b1ba9eb697073067e79b6ff08c328372061464f
dimkalobov95 <dimkalobov95@gmail.com>
2019-12-04 21:10:50 +0200

    additional information about blog/tours and new images

--- src/app/pages/blog/blog.module.ts
+++ src/app/pages/blog/blog.module.ts
@@ -4,7 +4,11 @@ import { BlogListComponent } from './blog-list/blog-list.component';
 import { BlogItemComponent } from './blog-item/blog-item.component';
 import { BlogPageComponent } from './blog-page/blog-page.component';
 
-
+// take a look into
+// https://www.garmin.com/en-US/blog/fitness/
+// and
+// https://www.garmin.com/en-US/blog/fitness/tacx-training-app/
+// for example
 
 @NgModule({
   declarations: [BlogListComponent, BlogItemComponent, BlogPageComponent],
--- src/app/pages/tours/tours.module.ts
+++ src/app/pages/tours/tours.module.ts
@@ -5,6 +5,12 @@ import { TourItemComponent } from './tour-item/tour-item.component';
 import { TourPageComponent } from './tour-page/tour-page.component';
 import {RouterModule} from '@angular/router';
 
+//take a look into
+//tourradar.com - list of tours(first part of a page)
+// and
+// https://www.tourradar.com/t/185437
+// for example
+
 @NgModule({
   declarations: [ToursListComponent, TourItemComponent, TourPageComponent],
   imports: [
