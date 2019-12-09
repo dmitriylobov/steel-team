0d596eea0083e45716ab33419271389b0e854f72
dmitriylobov <dimkalobov95@gmail.com>
2019-12-09 19:17:53 +0200

    component

---
+++ src/app/features/card/rotated-card/rotated-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rotated-card',
+  templateUrl: './rotated-card.component.html',
+  styleUrls: ['./rotated-card.component.scss']
+})
+export class RotatedCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
