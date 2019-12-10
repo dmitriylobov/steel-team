5e75ed5040e8a1da812e1de589cee68e2a0583a6
dmitriylobov <dimkalobov95@gmail.com>
2019-12-10 15:28:59 +0200

    deploy command finished

--- src/app/features/header/language-selector/languages.ts
+++ src/app/features/header/language-selector/languages.ts
@@ -1,6 +1,6 @@
 import {Language} from "src/models/language";
 
 export const LANGUAGES:Language[] = [
-  {id: 'RUS', imageUrl: '/assets/lang/rus.svg'},
-  {id: 'EN', imageUrl: '/assets/lang/en.svg'}
+  {id: 'RUS', imageUrl: 'assets/lang/rus.svg'},
+  {id: 'EN', imageUrl: 'assets/lang/en.svg'}
 ];
