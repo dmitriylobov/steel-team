b7485e5f418be6d05e2369435e16daf3cf0821c2
dmitriylobov <dimkalobov95@gmail.com>
2019-12-20 18:03:34 +0200

    mat-expansion tour

--- src/app/pages/tours/tours.module.ts
+++ src/app/pages/tours/tours.module.ts
@@ -7,6 +7,8 @@ import {RouterModule} from '@angular/router';
 import {ToursMainComponent} from './tours-main/tours-main.component';
 import {GalleryModule} from 'src/app/features/gallery/gallery.module';
 import {AgmCoreModule} from '@agm/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
 
 //  take a look into
 //  tourradar.com - list of tours(first part of a page)
@@ -26,6 +28,8 @@ import {AgmCoreModule} from '@agm/core';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCcGAOo7lbNi93Vi6sd9EAyOo8o03so698'
     }),
+    MatExpansionModule,
+    MatIconModule,
   ]
 })
 export class ToursModule {
