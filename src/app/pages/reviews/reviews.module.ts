34e2713658199880e778dad60cf84434770cd11c
dmitriylobov <dimkalobov95@gmail.com>
2019-12-26 16:44:48 +0200

    pointers

--- src/app/pages/reviews/reviews.module.ts
+++ src/app/pages/reviews/reviews.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReviewComponent } from './review/review.component';
-import { ReviewsListComponent } from './reviews-list/reviews-list.component';
-import { ReviewsPageComponent } from './reviews-page/reviews-page.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReviewComponent} from './review/review.component';
+import {ReviewsListComponent} from './reviews-list/reviews-list.component';
+import {ReviewsPageComponent} from './reviews-page/reviews-page.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [ReviewComponent, ReviewsListComponent, ReviewsPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', pathMatch: 'full', component: ReviewsPageComponent},
+    ]),
   ]
 })
-export class ReviewsModule { }
+export class ReviewsModule {
+}
