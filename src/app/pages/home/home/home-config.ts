1404ab8720a7925fc3f5616ee3b1b7df541a604c
dmitriylobov <dimkalobov95@gmail.com>
2020-01-29 10:20:53 +0200

    new main screen

--- src/app/pages/home/home/home-config.ts
+++ src/app/pages/home/home/home-config.ts
@@ -2,53 +2,12 @@ import {LandingConfig} from './landing-config';
 
 export const HOME_CONFIG: LandingConfig[] = [
   {
-    backgroundImage: 'assets/main.png',
-    heading: 'Toha lord of the winterfall',
-    subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    // tslint:disable-next-line:max-line-length
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    backgroundImage: 'assets/main-image.jpg',
+    heading: 'Поехали с нами!',
+    subheading: 'Фитнесс тур с командой проффесионалов',
+    text: 'Хорошая атмосфера. Дружная компания, интересные места. Отдых в комании единомышленников с пользой для здоровья!',
     url: '/tours',
     height: '700px',
-    linkName: 'To Fight!'
-  },
-  {
-    backgroundImage: 'assets/fit-main.png',
-    heading: 'Lorem ipsum dolor sit amet',
-    subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    // tslint:disable-next-line:max-line-length
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    url: '/tours',
-    height: '900px',
-    linkName: 'To Fight!'
-  },
-  {
-    backgroundImage: 'assets/sea-main.png',
-    heading: 'Lorem ipsum dolor sit amet',
-    subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    // tslint:disable-next-line:max-line-length
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    url: '/tours',
-    height: '1000px',
-    linkName: 'To Fight!'
-  },
-  {
-    backgroundImage: 'assets/trainhard.jpg',
-    heading: 'Lorem ipsum dolor sit amet',
-    subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    // tslint:disable-next-line:max-line-length
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    url: '/tours',
-    height: '800px',
-    linkName: 'To Fight!'
-  },
-  {
-    backgroundImage: 'assets/rockgirl.webp',
-    heading: 'Lorem ipsum dolor sit amet',
-    subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    // tslint:disable-next-line:max-line-length
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    url: '/tours',
-    height: '800px',
-    linkName: 'To Fight!'
+    linkName: 'Летим!'
   }
 ];
