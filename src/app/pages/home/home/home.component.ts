2d9deddfa14ac9822dce7b41e1afc7c8a098eb01
deathgun04 <darekz04111995@gmail.com>
2020-02-06 17:47:10 +0200

    no flex

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import {HOME_CONFIG} from './home-config';
+import { HOME_CONFIG } from './home-config';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
+
 })
 export class HomeComponent {
   screens = HOME_CONFIG;
 }
+
