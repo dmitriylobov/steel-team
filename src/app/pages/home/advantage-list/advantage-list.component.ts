4c412f5097027513e95d6451a238d1ca9d624c4a
deathgun04 <darekz04111995@gmail.com>
2020-02-06 17:19:37 +0200

    no flex

---
+++ src/app/pages/home/advantage-list/advantage-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advantage-list',
+  templateUrl: './advantage-list.component.html',
+  styleUrls: ['./advantage-list.component.scss']
+})
+export class AdvantageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
