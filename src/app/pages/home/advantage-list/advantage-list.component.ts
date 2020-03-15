75403b6e95befba21dfd7e3da3aec3232105644d
deathgun04 <darekz04111995@gmail.com>
2020-03-15 20:42:33 +0200

    (finish

--- src/app/pages/home/advantage-list/advantage-list.component.ts
+++ src/app/pages/home/advantage-list/advantage-list.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-advantage-list',
   templateUrl: './advantage-list.component.html',
   styleUrls: ['./advantage-list.component.scss']
 })
-export class AdvantageListComponent implements OnInit {
-
-
-  ngOnInit() {
-  }
-
+export class AdvantageListComponent {
 }
+
